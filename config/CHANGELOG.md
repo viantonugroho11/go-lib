a53bcfb230c8b0c7f8adeadb5533779b6d530431
Muhammad Novianto Nugroho <novianto.nug@gmail.com>
2026-01-03 20:09:49 +0700

    docs(config): update example config

--- config/CHANGELOG.md
+++ config/CHANGELOG.md
@@ -1,3 +1,7 @@
-
 ## config/v0.1.0 - 2026-01-03
+
+### Changes
+
+- **docs:** update readme and update comment functional
+- 
 
--- xlog/example/main.go
+++ xlog/example/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"time"
 	xlog "xlog"
 )
 
@@ -10,7 +11,7 @@ func main() {
 	_, cleanup := xlog.MustInitFromEnv()
 	defer cleanup()
 
-	// Inject field dari context (opsional)
+	// Inject context field extractor
 	xlog.SetContextFieldExtractor(func(ctx context.Context) []xlog.Field {
 		if v := ctx.Value("request_id"); v != nil {
 			return []xlog.Field{xlog.Str("request_id", v.(string))}
@@ -20,13 +21,22 @@ func main() {
 
 	ctx := context.WithValue(context.Background(), "request_id", "req-123")
 
-	// Contoh pemakaian sugared (kv-style)
+		// example of sugared (kv-style)
 	xlog.SugaredLogger().Infow("service started", "module", "example", "feature", "xlog")
 
-	// Contoh pemakaian structured
+	// example of structured log
 	xlog.Logger().Info("structured log", xlog.Str("module", "example"))
 
-	// Contoh pemakaian fungsi berbasis context
+	// example of any log
+	xlog.Logger().Info("any log", xlog.Any("any", "any"))
+
+	// example of time log
+	xlog.Logger().Info("time log", xlog.Time("time", time.Now()))
+
+	// example of duration log
+	xlog.Logger().Info("duration log", xlog.Dur("duration", 1*time.Second))
+
+	// example of error log
 	err := errors.New("something went wrong")
 	xlog.Error(ctx, "failed to process", xlog.Err(err))
 }
